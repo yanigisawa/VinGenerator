6de9f2cbac65aa0c5c7c49378fe251e6c01554a9
James Alexander <jamesralexander@gmail.com>
2011-02-19 00:05:12 -0500

    Initial check-in of the VIN Generator

---
+++ num.py
@@ -0,0 +1,30 @@
+#!/usr/local/bin/python
+
+import datetime;
+import cgi;
+import cgitb; cgitb.enable();  # for troubleshooting
+import vin
+
+def PrintHeaders():
+	print "Content-Type: text/html" # HTML is following
+	print # blank line, end of headers
+	print "<html><body>"
+	print """<meta name="viewport" content="width=440, initial-scale=1.0, user-scalable=no"/>"""
+
+
+def PrintFooter():
+	print "</body></html>"
+
+
+def Main():
+
+	PrintHeaders()
+
+	print "<strong>Random Vins:</strong><p/>"
+	for i in range(5):
+		print "<p>%s</p>" % (vin.getRandomVin())
+
+
+	PrintFooter()
+
+Main()
